04cb7096c78c5c562c563452af3e6b21c2bb2605
penkinsergey <penkinsergeykms@gmail.com>
2023-12-20 22:45:54 +0800

    Commit 1 vetka1

--- src/main/java/ru/java/basic/penkin/hw14/SingleThreadedArrayFill.java
+++ src/main/java/ru/java/basic/penkin/hw14/SingleThreadedArrayFill.java
@@ -1,2 +1,16 @@
-package ru.java.basic.penkin.hw14;public class SingleThreadedArrayFill {
+package ru.java.basic.penkin.hw14;
+
+public class SingleThreadedArrayFill {  // На гитхаб vetka1
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+
+        double[] array = new double[100_000_000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+        }
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - time;
+        System.out.println("Время выполнения при одном потоке: " + executionTime + " мс");
+    }
 }
